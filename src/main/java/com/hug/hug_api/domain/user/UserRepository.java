f8fcaf2dde3489c5611b2ab4bb3a000eb4bdbc20
JeongHoLim <9997ijh@gmail.com>
2022-05-03 01:10:01 +0900

    feat : UserRepository, UserService 추가
    username은 email로 설정
    
    #2

--- src/main/java/com/hug/hug_api/domain/user/UserRepository.java
+++ src/main/java/com/hug/hug_api/domain/user/UserRepository.java
@@ -2,5 +2,10 @@ package com.hug.hug_api.domain.user;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<User,String> {
+
+    Optional<User> findByEmail(String email);
+
 }
