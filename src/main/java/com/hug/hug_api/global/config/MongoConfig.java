a04b348b517778c7a7b534374314300895a94fe1
JeongHoLim <9997ijh@gmail.com>
2022-05-03 23:10:47 +0900

    feat : mongoDB config 추가
    transaction을 위한 configuration 추가
    
    #1

---
+++ src/main/java/com/hug/hug_api/global/config/MongoConfig.java
@@ -0,0 +1,38 @@
+package com.hug.hug_api.global.config;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+public class MongoConfig {
+
+    @Value("${spring.data.mongodb.host}")
+    private String host;
+
+    @Value("${spring.data.mongodb.port}")
+    private String port;
+
+    @Bean
+    public MongoClient mongo() {
+        String uri = "mongodb://" + host + ":" + port + "/hug";
+
+        ConnectionString connectionString = new ConnectionString(uri);
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+
+        return MongoClients.create(mongoClientSettings);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() throws Exception {
+        return new MongoTemplate(mongo(), "hug");
+    }
+}
