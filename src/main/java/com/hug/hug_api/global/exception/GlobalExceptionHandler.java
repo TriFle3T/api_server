cb85e76e2eadcf23d6b96f186e669eead22526d5
JeongHoLim <9997ijh@gmail.com>
2022-05-04 04:14:42 +0900

    fix : excpetion handling
    auth, bad request 관련 수정
    
    #10

--- src/main/java/com/hug/hug_api/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hug/hug_api/global/exception/GlobalExceptionHandler.java
@@ -2,9 +2,11 @@ package com.hug.hug_api.global.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.HttpClientErrorException;
 
 @Slf4j
 @RestControllerAdvice
@@ -15,11 +17,27 @@ public class GlobalExceptionHandler {
         log.error(e.getErrorCode().getDetail(),e.getErrorCode().getHttpStatus());
         return ErrorResponse.toResponseEntity(e);
     }
+    @ExceptionHandler(value = {HttpClientErrorException.BadRequest.class, HttpMessageNotReadableException.class})
+    protected ResponseEntity<ErrorResponse> handleBadException(Exception e){
+        var ce = CustomException.builder().errorCode(ErrorCode.INVALID_ACCESS).build();
+        log.error(ce.getErrorCode().getDetail(),ce.getErrorCode().getHttpStatus());
+        return ErrorResponse.toResponseEntity(ce);
+    }
 
-    @ExceptionHandler(value = {Exception.class})
+
+    @ExceptionHandler(value = {BadCredentialsException.class})
+    protected ResponseEntity<ErrorResponse> handleAuthenticationException(Exception e){
+        var ce = CustomException.builder().errorCode(ErrorCode.INVALID_AUTH).build();
+        log.error(ce.getErrorCode().getDetail(),ce.getErrorCode().getHttpStatus());
+        return ErrorResponse.toResponseEntity(ce);
+    }
+
+
+    @ExceptionHandler(value = {RuntimeException.class})
     protected ResponseEntity<ErrorResponse> handleInternalServerError(Exception e){
         var ce = CustomException.builder().errorCode(ErrorCode.SERVER_ERROR).build();
         log.error(ce.getErrorCode().getDetail(),ce.getErrorCode().getHttpStatus());
         return ErrorResponse.toResponseEntity(ce);
     }
+
 }
