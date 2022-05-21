94c3c8fccbea88d49ebae0ce56fa10ce1a1204f2
JeongHoLim <9997ijh@gmail.com>
2022-05-21 20:24:04 +0900

    fix : 격언 랜덤 인덱스 버그, 격언 중복 삽입 방지

--- src/main/java/com/hug/hug_api/domain/quote/domain/Quote.java
+++ src/main/java/com/hug/hug_api/domain/quote/domain/Quote.java
@@ -4,6 +4,7 @@ import com.hug.hug_api.domain.quote.dto.SingleQuoteDto;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
@@ -12,9 +13,13 @@ import java.util.List;
 @Data
 @Builder
 public class Quote {
+
     @Id
     private String id;
+
+    @Indexed(unique = true)
     private int index;
+
     private String theme;
     private List<SingleQuoteDto> quotes;
 
