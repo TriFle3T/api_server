742ddfe213a1931dc9865f3bcb9121d08ed8b588
JeongHoLim <9997ijh@gmail.com>
2022-05-24 23:45:08 +0900

    refactor : error timestamp 형식 변경
    LocalDateTime -> String ("YYYY-MM-DD")

--- src/main/java/com/hug/hug_api/global/common/ErrorResponse.java
+++ src/main/java/com/hug/hug_api/global/common/ErrorResponse.java
@@ -4,12 +4,13 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @Builder
 public class ErrorResponse {
 
-    private final LocalDateTime timeStamp = LocalDateTime.now();
+    private final String timeStamp = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
     private String code;
     private String error;
 
